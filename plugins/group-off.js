2308e270a88f96b36259af7bb38a607f3bfd210b
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2023-06-20 00:31:04 +0700

    Update group-off.js

--- plugins/group-off.js
+++ plugins/group-off.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Uaaaaawh, Clara Baru Bangun Nih >,<* ')
+    m.reply(' *Uaaaaawh, Cycy Baru Bangun Nih >,<* ')
 }
 
 
