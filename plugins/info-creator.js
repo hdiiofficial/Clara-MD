21617f48c02768e059a72a95f089a919780f276b
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2023-06-20 00:32:32 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,4 +1,4 @@
-//* import fetch from 'node-fetch'
+*// import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
@@ -14,4 +14,4 @@ handler.help = ['owner', 'creator']
 handler.tags = ['main', 'info']
 handler.command = /^(owner|creator)/i
 export default handler
-*//
+//*
