9fd30a04170d49a0ef11cb2b0d0821945227a7e1
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2023-06-20 00:30:44 +0700

    Update group-on.js

--- plugins/group-on.js
+++ plugins/group-on.js
@@ -1,7 +1,7 @@
 let handler = async (m, { participants }) => {
     // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply(' *Huwaaa, Clara Mau Tidur Dulu >,<* ')
+    m.reply(' *Huwaaa, Cycy Mau Tidur Dulu >,<* ')
     // } else m.reply('Ada nomor Ownerku disini...')
 }
 handler.command = /^(grupoff)$/i
